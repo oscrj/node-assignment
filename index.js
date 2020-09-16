539dc4a1551b8965082f3671540e554d00ef10e8
Oscar Johanneson <oscar@Oscars-MacBook-Pro.local>
2020-09-16 15:05:31 +0200

    Added params value

--- index.js
+++ index.js
@@ -3,10 +3,26 @@ const url = require('url');
 const fs = require('fs');
 const port = 8080;
 
-var server = http.createServer(respond).listen(port, () => {
+// Create webserver on port 8080 and use a callback-function for request and response.
+http.createServer(respond).listen(port, () => {
     console.log("Server running at port: " + port);    
 });
 
 function respond(request, response){
-    response.end();
+    let params = url.parse(request.url, true);
+    let key = "theKey";
+
+    if(params.query.key == key){
+        response.write('U R IN');
+        response.end();
+    }else{
+        fs.readFile('./wishlist.txt', {encoding: 'utf8'}, (err, txtFile) => {
+            if(err){
+                alert(err.message);
+            }else{
+                response.write(txtFile);
+            }
+            response.end();
+        });
+    }
 }
